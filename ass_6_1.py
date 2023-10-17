a241ef0ea671418659658f7102d32591fdd8b600
xarafus <125128286+xarafus@users.noreply.github.com>
2023-10-17 20:47:31 +0600

    ass_6_1.py

--- ass_6_1.py
+++ ass_6_1.py
@@ -1,9 +1,8 @@
 fname = input("Enter a file name: ")
-
 try:
-    with open(fname, 'r') as fhand:
-        for line in fhand:
-            print(line.upper().rstrip())
+    fhand = open(fname)
+    for line in fhand:
+        print(line.upper().rstrip())
 except FileNotFoundError:
     print(f"File not found: {fname}")
 except Exception as e:
